f059eff14129a65519130965a27913ec86e69eb1
selcukdevtr <selcuk.dev.tr@gmail.com>
2025-12-26 17:53:48 +0300

    test1
    text1

--- scrtips.js
+++ scrtips.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Sayfa kaydırıldığında butonu göster/gizle
 window.onscroll = function () {
   const upButton = document.querySelector(".content__up--link");
@@ -13,3 +14,16 @@ window.onscroll = function () {
 
 // Yumuşak geçiş zaten HTML'de 'scroll-behavior: smooth' ile tanımlı,
 // o yüzden ekstra bir JS animasyonuna gerek yok.
+=======
+window.onscroll = function () {
+  const upButton = document.querySelector(".content__up--link");
+  if (
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    upButton.style.display = "block";
+  } else {
+    upButton.style.display = "none";
+  }
+};
+>>>>>>> Stashed changes
